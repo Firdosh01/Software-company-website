ae8c45b34e83acd7b4465c14b86726c970080666
Firdosh01 <firdosh7392@gmail.com>
2023-11-13 23:23:28 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/common/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Services from './components/Pages/Services';
+import Portfolio from './components/Pages/Portfolio';
+import Careers from './components/Pages/Careers';
 
 function App() {
   return (
     <div className='w-screen min-h-screen bg-img'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='services' element={<Services />} />
+        <Route path='portfolio' element={<Portfolio />} />
+        <Route path='/careers' element={<Careers />} />
+      </Routes>
     </div>
   );
 }
