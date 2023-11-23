543f22cd7e3b6a0569dad96a1fd02c7850a80240
Firdosh01 <firdosh7392@gmail.com>
2023-11-23 03:51:20 -0500

    little update internship page

--- src/components/Pages/Internship.jsx
+++ src/components/Pages/Internship.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
-import firstImg from '../../assets/34.jpg'
-import secondImg from '../../assets/man.jpg'
+import React from "react";
+import firstImg from "../../assets/34.jpg";
+import secondImg from "../../assets/man.jpg";
 export default function Internship() {
   return (
     <div>
       <div className="relative w-11/12 max-w-screen-xl px-1 mx-auto md:px-10">
-        <div className='flex flex-col justify-between gap-5 py-16 lg:items-center lg:flex-row'>
-        
-        <div className="lg:w-[50%] relative">
-            <span className=" ml-7 about-arrow text-[#FD0F00]">INTERNSHIP</span>
-            <h1 className="sm:text-4xl font-semibold lg:leading-[80px]  text-white lg:text-7xl md:text-5xl pt-5">
-            Nurturing Youth In The Digital Field.
-
+        <div className="flex flex-col justify-between gap-5 py-16 lg:flex-row lg:items-center">
+          <div className="lg:w-[50%] relative">
+            <span className=" ml-7 about-arrow text-[#FD0F00] ">
+              INTERNSHIP
+            </span>
+            <h1 className="text-4xl font-semibold lg:leading-[80px]  text-white lg:text-7xl md:text-5xl pt-5">
+              Nurturing Youth In The Digital Field.
             </h1>
             <p className="py-5 text-sm font-medium text-white md:text-base">
-            Experience day-by-day work culture includes marketing, market research, software/tool and managing the social media .as well as Website.
+              Experience day-by-day work culture includes marketing, market
+              research, software/tool and managing the social media .as well as
+              Website.
             </p>
           </div>
-          
-          <div>
-            <div className='flex lg:w-[50%]'>
-            <img src={secondImg} alt="" className='h-[350px] h[450px] w-full object-cover bg-center' />
-              <img src={firstImg} alt=""  className='h-[350px] w[800px] h[450px]  w-full  object-cover bg-center' />
+
+          <div  className=" lg:w-[50%] relative">
+            <div className="flex">
+              <img
+                src={secondImg}
+                alt=""
+                className="object-cover max-w-[50%] md:h-[100%]"
+              />
+              <img
+                src={firstImg}
+                alt=""
+                className="object-cove max-w-[50%] md:h-[100%] bg-center"
+              />
+            </div>
+            <div className="absolute bg-[#FD0F00] text-center py-3 px-7 text-white md:-bottom-9 -bottom-16 left-[20%]">
+              <h1 className="font-semibold text-7xl">7</h1>
+              <span className="uppercase ">Years of experience</span>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
-  )
+  );
 }
